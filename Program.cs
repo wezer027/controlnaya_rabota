deb476a5f4c933b18df4d094d883268b9ae65f7e
wezerPC <moskalenko.96@inbox.ru>
2023-05-12 12:26:23 +0300

    создал папку readme.md создал половину кода для решения задачи

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] array1 = new string[5] {"Hello", "123", "world", "12345", "compromises"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
